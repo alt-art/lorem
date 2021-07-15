1a1741ae141fd86634dc35df7e630a3fc1cbaca5
altart <pedromendescraft@gmail.com>
2021-07-15 20:26:48 -0300

    Update gtk-rs to 0.14.0

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,9 @@ extern crate gdk;
 extern crate glib;
 
 use gtk::prelude::*;
-use gio::prelude::*;
 use glib::clone;
 
 use gtk::{ApplicationWindow, Builder, Label, Button, Adjustment, Clipboard};
-
-use std::env::args;
 
 mod lorem;
 use lorem::Lorem;
@@ -24,18 +21,18 @@ fn build_ui(application: &gtk::Application) {
     let glade_src = include_str!("./resources/lorem.glade");
     let builder = Builder::from_string(glade_src);
 
-    let window: ApplicationWindow = builder.get_object("window").expect("Cound't get ApplicationWindow");
-    let label: Label = builder.get_object("label").expect("Cound't get Label");
-    let min_adjustment: Adjustment = builder.get_object("min_adjust").expect("Cound't get Adjustment");
-    let max_adjustment: Adjustment = builder.get_object("max_adjust").expect("Cound't get Adjustment");
-    let gen_button: Button = builder.get_object("gen").expect("Cound't get Button");
+    let window: ApplicationWindow = builder.object("window").expect("Cound't get ApplicationWindow");
+    let label: Label = builder.object("label").expect("Cound't get Label");
+    let min_adjustment: Adjustment = builder.object("min_adjust").expect("Cound't get Adjustment");
+    let max_adjustment: Adjustment = builder.object("max_adjust").expect("Cound't get Adjustment");
+    let gen_button: Button = builder.object("gen").expect("Cound't get Button");
     gen_button.connect_clicked(clone!(@weak label => move |_| {
-        generate(label, min_adjustment.get_value() as u32, max_adjustment.get_value() as u32);
+        generate(label, min_adjustment.value() as u32, max_adjustment.value() as u32);
     }));
-    let copy_button: Button = builder.get_object("copy").expect("Cound't get Button");
+    let copy_button: Button = builder.object("copy").expect("Cound't get Button");
     copy_button.connect_clicked(clone!(@weak label => move |_| {
         let clipboard = Clipboard::get(&gdk::SELECTION_CLIPBOARD);
-        clipboard.set_text(&label.get_text());
+        clipboard.set_text(&label.text());
     }));
     generate(label, 2, 3);
     window.set_application(Some(application));
@@ -46,10 +43,10 @@ fn main() {
     let application = gtk::Application::new(
         Some("com.github.altart.lorem"),
         Default::default(),
-    ).expect("Initialization failed");
+    );
     application.connect_activate(|app| {
         build_ui(app)
     });
 
-    application.run(&args().collect::<Vec<_>>());
+    application.run();
 }
